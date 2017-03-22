5c1391e59c431cc1d0f5c5a8047fd75bb2764129
Orion Henry <orion.henry@gmail.com>
2017-03-22 14:04:59 -0400

    redux

--- src/app.jsx
+++ src/app.jsx
@@ -1,19 +1,20 @@
 
 let React       = require('react');
-let ReactDOM    = require("react-dom");
+let ReactDOM    = require('react-dom');
+let Redux       = require('redux');
+let reducer     = require('./reducer');
 
 class Foo extends React.Component {
-//const Foo = () => ({
   constructor(props) {
     super(props)
   }
 
   render() {
     return (<div className="foo"> {this.props.content} </div>);
-    //return null;
   }
 }
 
+let Store = Redux.createStore(reducer);
 let content = "react initializing...";
 let mount = document.getElementById("mount");
 console.log("Mount node", mount);
---
+++ src/reducer.js
@@ -0,0 +1,11 @@
+
+const action = (state = [], action) => {
+  switch (action.type) {
+    case 'LOG':
+      return [ ...state, action.text ]
+    default:
+      return state
+  }
+}
+
+export default action
